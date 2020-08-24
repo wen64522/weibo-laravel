09c6ace22798e77cbefe02621d66f6270c89866c
wenyw <645228905@qq.com>
2020-08-24 09:16:05 +0000

    add header and footer

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+ <img class="brand-icon" src="https://cdn.learnku.com/uploads/images/201908/22/1/oVrAoOQ4Vc.png!/fw/40" style="height: 20px;width: 20px;">
+ <a href="https://laravel-china.org/courses" target=_blank>
+ 刻意练习，每日精进
+ </a>
+ <div class="float-right">
+ <a href="/about" >关于</a>
+ </div>
+</footer>
