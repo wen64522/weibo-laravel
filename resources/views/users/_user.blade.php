b9184fd7f7fedbb4d33671522fae4c4170793941
wenyw <645228905@qq.com>
2020-08-25 08:58:14 +0000

    delete user

--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -3,4 +3,11 @@
   <a href="{{ route('users.show',$user) }}">
     {{ $user->name }}
   </a>
+  @can('destroy',$user)
+  <form action="{{ route('users.destroy',$user->id) }}" method="POST" class="float-lg-right">
+    {{ csrf_field() }}
+    {{ method_field('DELETE') }}
+    <button class="btn btn-block btn-danger delete-btn" type="submit" name="button">删除</button>
+  </form>
+  @endcan
 </div>
