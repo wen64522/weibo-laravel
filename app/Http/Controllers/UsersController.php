81091e98d79c097377d8a4a828a2f306177306ff
wenyw <645228905@qq.com>
2020-08-25 06:48:38 +0000

    login nav

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,10 +6,11 @@ use Illuminate\Http\Request;
 
 use App\Models\User;
 
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
-        public function create()
+    public function create()
     {
         return view('users.create');
     }
@@ -20,21 +21,23 @@ class UsersController extends Controller
     }
 
     public function store(Request $request)
-{
- $this->validate($request, [
- 'name' => 'required|max:50',
- 'email' => 'required|email|unique:users|max:255',
- 'password' => 'required|confirmed|min:6'
- ]);
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
 
- $user = User::create([
- 'name' => $request->name,
- 'email' => $request->email,
- 'password' => bcrypt($request->password),
- ]);
- session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        Auth::login($user);
 
- return redirect()->route('users.show', [$user]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
-}
+        return redirect()->route('users.show', [$user]);
+
+    }
 }
